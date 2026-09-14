516c9733697d4939082829e68b4fb749c6461d1c
salsaramadhani <salsaramadhani249@gmail.com>
2026-09-14 13:32:33 +0700

    salsa-menambahkan route untuk riwayat

--- app/Modules/talents/routes.php
+++ app/Modules/talents/routes.php
@@ -9,6 +9,7 @@ Route::controller(talentsController::class)->middleware(['web','auth'])->name('t
 	Route::get('/talents/create', 'create')->name('create');
 	Route::post('/talents', 'store')->name('store');
 	Route::get('/talents/{talents}', 'show')->name('show');
+	Route::get('/talents/{talents}/riwayat', 'riwayat')->name('riwayat');
 	Route::get('/talents/{talents}/edit', 'edit')->name('edit');
 	Route::patch('/talents/{talents}', 'update')->name('update');
 	Route::get('/talents/{talents}/delete', 'destroy')->name('destroy');
